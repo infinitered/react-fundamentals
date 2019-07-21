27da13bf16df73dbda1f9c2674a482d3a784869c
Ryan Linton <ryan@infinite.red>
2019-07-21 15:33:25 -0700

    I love cats

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { createSwitchNavigator } from "@react-navigation/core";
+import { createBrowserApp } from "@react-navigation/web";
 import './App.css';
 
-function App() {
+function GalleryImage({ item, onClick }) {
+  return <img className="GalleryImage" src={item.url} height="300" onClick={onClick} />
+}
+
+function Gallery({ items, onClickItem }) {
+  return <div className="Gallery">
+    {items.map(i => <GalleryImage item={i} onClick={() => onClickItem(i)} />)}
+  </div>
+}
+
+function PhotoPickerScreen({ navigation }) {
+  const [cats, setCats] = useState([])
+  const [selectedCats, setSelectedCats] = useState([])
+
+  async function fetchData() {
+    const resp = await fetch("https://api.thecatapi.com/v1/images/search?limit=50")
+    resp.json().then(resp => setCats(resp))
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+
+  const selectImg = (imgData) => {
+    setSelectedCats([...selectedCats, imgData])
+    navigation.navigate("screen2")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Header">
+        <h1>Caturday Photo Picker</h1>
       </header>
+      <Gallery items={cats} onClickItem={selectImg} />
+      <aside className="Sidebar">
+        {selectedCats.map(c =>
+          <div className="SelectedImage">
+            <img className="GalleryImage" src={c.url} height="80" />
+            {c.description}
+          </div>
+        )}
+      </aside>
     </div>
   );
 }
+
+function Screen2() {
+  return <h1>Hello from screen 2</h1>
+}
+
+const AppNavigator = createSwitchNavigator({
+  photoPicker: PhotoPickerScreen,
+  screen2: Screen2,
+});
+
+const App = createBrowserApp(AppNavigator)
 
 export default App;
