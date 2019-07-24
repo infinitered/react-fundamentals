73dd19dc88dfef91eba82a3b485ad5022eaea376
Ryan Linton <ryan@infinite.red>
2019-07-24 08:09:13 -0700

    Hook up registration and login

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,18 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react'
 import { Button, Footer, Screen } from "../components"
 
-export const SignInScreen = () => {
+export const SignInScreen = ({ navigation }) => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const doSignIn = async () => {
+    if (!username.length || !password.length) {
+      alert("Both username and password are required.")
+      return
+    }
+
+    const resp = await fetch("http://localhost:2403/parents/login", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    })
+
+    if (resp.ok) {
+      const { id } = await resp.json()
+      localStorage.setItem('sid', id)
+      navigation.navigate("feed")
+    } else {
+      alert("Incorrect credentials please try again.")
+    }
+  }
+
   return (
     <Screen footer={
       <Footer>
-        <Button>
+        <Button onClick={doSignIn}>
           Sign In
         </Button>
       </Footer>
     }>
       <form>
-        <input placeholder="Username" />
-        <input placeholder="Password" />
+        <input
+          type="email"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
       </form>
     </Screen>
   );
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,20 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Footer, Screen } from "../components"
 
-export const SignUpScreen = () => {
+export const SignUpScreen = ({ navigation }) => {
+  const campId = navigation.getParam("campId")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [name, setName] = useState("")
+  const [childName, setChildName] = useState("")
+
+  const doSignUp = async () => {
+    if (!username.length || !password.length || !name.length || !childName.length) {
+      alert("All fields are required.")
+      return
+    }
+
+    const resp = await fetch("http://localhost:2403/parents", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        name,
+        childName,
+        registeredCamp: campId
+      })
+    })
+
+    if (resp.ok) {
+      const authResp = await fetch("http://localhost:2403/parents/login", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          password
+        })
+      })
+
+      if (authResp.ok) {
+        const { id } = await authResp.json()
+        localStorage.setItem('sid', id)
+        navigation.navigate("feed")
+      } else {
+        alert("Unable to authenticate.")
+      }
+    } else {
+      alert("Unable to register.")
+    }
+  }
+
   return (
     <Screen footer={
       <Footer>
-        <Button>
+        <Button onClick={doSignUp}>
           Sign Up
         </Button>
       </Footer>
     }>
       <form>
-        <input placeholder="Username" />
-        <input placeholder="Password" />
-        <input placeholder="Your name" />
-        <input placeholder="Child's name" />
+        <input
+          type="email"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <input
+          type="text"
+          placeholder="Your name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          type="text"
+          placeholder="Child's name"
+          onChange={(e) => setChildName(e.target.value)}
+          value={childName}
+        />
       </form>
     </Screen>
   );
