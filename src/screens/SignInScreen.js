10b4ce5460f8489b52079a9a030996cedf8c0d41
Ryan Linton <ryan@infinite.red>
2019-07-25 21:05:50 -0700

    Update to controlled input components and setup auth request

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -5,27 +5,53 @@ export const SignInScreen = ({ navigation }) => {
   const [ username, setUsername ] = useState("")
   const [ password, setPassword ] = useState("")
 
-  const doSignIn = () => alert("doSignIn")
+  const doSignIn = async () => {
+    if (!username.length || !password.length) {
+      alert("Both username and password are required.")
+      return
+    }
+
+    const resp = await fetch("https://campminder-training-api.herokuapp.com/parents/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    })
+
+    if (resp.ok) {
+      // TODO: Do something with token
+      navigation.navigate("feed")
+    } else {
+      alert("Incorrect credentials please try again.")
+    }
+  }
 
   return (
     <Screen footer={
       <Footer>
         <Button onClick={doSignIn}>
-          Sign in as {username} with {password}
+          Sign In
         </Button>
       </Footer>
     }>
-      <form>
+      <form onSubmit={(e) => { e.preventDefault(); doSignIn() }}>
         <input
           type="email"
           placeholder="Username"
           onChange={(e) => setUsername(e.target.value)}
+          value={username}
         />
         <input
           type="password"
           placeholder="Password"
           onChange={(e) => setPassword(e.target.value)}
+          value={password}
         />
+        <input type="submit" hidden />
       </form>
     </Screen>
   )
