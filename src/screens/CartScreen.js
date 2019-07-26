4f86b05919b892a7add0cec759d8c32a134080ff
Ryan Linton <ryan@infinite.red>
2019-07-26 12:16:31 -0700

    Sync state with localStorage and add more cart actions

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,12 +1,47 @@
 import React, { useContext } from "react"
-import { NavBar, Post, Screen } from "../components"
+import { observer } from "mobx-react"
+import { Button, Footer, NavBar, Post, Screen } from "../components"
 import { StoreContext } from "../App"
 
-export const CartScreen = () => {
-  const { cartItems } = useContext(StoreContext)
+export const CartScreen = observer(({ navigation }) => {
+  const { cartItems, clearCart, removeCartItem, token, total } = useContext(StoreContext)
+
+  const doPurchase = async () => {
+    const resp = await fetch("https://campminder-training-api.herokuapp.com/purchases", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token
+      },
+      body: JSON.stringify({
+        purchasedPosts: cartItems
+      })
+    })
+
+    if (resp.ok) {
+      clearCart()
+      navigation.navigate("purchases")
+    } else {
+      alert("Unable to purchase.")
+    }
+  }
 
   return (
-    <Screen>
+    <Screen
+      footer={
+        <Footer>
+          {cartItems.length > 0 &&
+            <div className="row content">
+              <Button onClick={clearCart}>
+                Clear Cart
+              </Button>
+              <Button onClick={doPurchase}>
+                Purchase {cartItems.length} {cartItems.length === 1 ? "item" : "items"} for ${total}
+              </Button>
+            </div>
+          }
+        </Footer>
+      }>
       <NavBar />
       <div className="content underNav">
         {cartItems.length === 0 &&
@@ -21,9 +56,11 @@ export const CartScreen = () => {
             title={i.title}
             imageUrl={i.imageUrl}
             subtotal={`$${(Number(i.quantity) * Number(i.price)).toFixed(2)}`}
+            actionText="Remove"
+            onClick={() => removeCartItem(i)}
           />
         ))}
       </div>
     </Screen>
   )
-}
+})
--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { types } from "mobx-state-tree"
+import { onSnapshot, types } from "mobx-state-tree"
 
 const Item = types.model({
   title: types.string,
@@ -8,18 +8,46 @@ const Item = types.model({
   id: types.string,
 })
 
-const Store = types
+export const Store = types
   .model({
     cartItems: types.array(Item),
     token: types.maybe(types.string),
   })
+  .views(self => ({
+    get total() {
+      const sum = self.cartItems.reduce(
+        (acc, item) => acc + Number(item.price) * Number(item.quantity),
+        0
+      )
+      return sum.toFixed(2)
+    }
+  }))
   .actions(self => ({
     addPostToCart(item) {
       self.cartItems.push(item)
     },
+    removeCartItem(item) {
+      const items = self.cartItems.filter(i => i.id !== item.id)
+      self.cartItems.replace(items)
+    },
     setToken(token) {
       self.token = token
     },
+    clearCart() {
+      self.cartItems.replace([])
+    },
+    reset() {
+      this.clearCart()
+      self.token = undefined
+    }
   }))
 
-export const store = Store.create({})
+const storedData = localStorage.getItem("store")
+const data = storedData ? JSON.parse(storedData) : {}
+
+export const store = Store.create(data)
+
+// track state in local storage
+onSnapshot(store, snapshot => {
+  localStorage.setItem("store", JSON.stringify(snapshot))
+})
